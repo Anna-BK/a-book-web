91ebf5e162f1290ae0c2a7a01a3047a9d09dc8ef
anna <te1902@gmail.com>
2023-02-19 16:47:52 +0900

    #1 style. NavBar - Menu (2023.02.19)

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,16 @@ function App() {
 
       </header>
       <div className="container">
-          <nav></nav>
-          <main></main>
+        <nav id="nav_snb">
+          <div className="list_menu">
+            <ul>
+              <li className="active">Active Menu</li>
+              <li>Inactive Menu</li>
+            </ul>
+          </div>
+
+        </nav>
+        <main></main>
       </div>
     </div>
   );
