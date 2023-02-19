25cce6d55da1044c1340f165289c860e8699f1f3
anna <te1902@gmail.com>
2023-02-19 22:14:36 +0900

    #1 style. Header - Profile (2023.02.19)

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <header className="header">
-
+      <header id="top_header" className="header">
+          <div className="login_group">
+            <div id="profile">
+              <a href="javascript:;">
+                <img id="gnb_profile_img" src="https://ssl.pstatic.net/static/common/myarea/myInfo.gif" height={26}/>
+                <span id="gnb_profile_name" >복이누나</span>
+              </a>
+              <div id="profile_layer" className="naver_theme">
+              <ul>
+                <li className="active">Settings</li>
+                <li>Logout</li>
+              </ul>
+              </div>
+            </div>
+          </div>
       </header>
       <div className="container">
         <nav id="nav_snb">
-          <div className="list_menu">
+          <div className="list_menu naver_theme">
             <ul>
               <li className="active">Active Menu</li>
               <li>Inactive Menu</li>
