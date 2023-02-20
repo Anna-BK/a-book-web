b46bda3c4aedde8727bf7c425c1c00346f704eaf
anna <te1902@gmail.com>
2023-02-21 00:09:25 +0900

    #1 style. 중간 커밋 (2023.02.20):

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,35 @@ function App() {
   return (
     <div className="app">
       <header id="top_header" className="header">
-          <div className="login_group">
-            <div id="profile">
-              <a href="javascript:;">
-                <img id="gnb_profile_img" src="https://ssl.pstatic.net/static/common/myarea/myInfo.gif" height={26}/>
-                <span id="gnb_profile_name" >복이누나</span>
-              </a>
-              <div id="profile_layer" className="naver_theme">
+        <div className="login_group">
+          <div id="profile">
+            <a href="javascript:;">
+              <img id="gnb_profile_img" src="https://ssl.pstatic.net/static/common/myarea/myInfo.gif" height={26} />
+              <span id="gnb_profile_name" >복이누나</span>
+            </a>
+            <div id="profile_layer" className="naver_theme">
               <ul>
                 <li className="active"><a>Settings</a></li>
                 <li><a>Logout</a></li>
               </ul>
-              </div>
             </div>
           </div>
+        </div>
       </header>
       <div className="container">
-        <nav id="nav_snb">
+        <nav id="nav_snb" className='container_item'>
           <div className="list_menu naver_theme">
             <ul>
               <li className="active"><a>Active Menu</a></li>
               <li><a>Inactive Menu</a></li>
             </ul>
           </div>
-
         </nav>
-        <main></main>
+        <main className='container_item'>
+            <div id="content">
+              <div id="content_header"><h3>타이틀</h3></div>
+            </div>
+        </main>
       </div>
     </div>
   );
