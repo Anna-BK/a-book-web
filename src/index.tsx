ea514e36274242ac6d0a647fe5c8fb64c7e6b684
anna <te1902@gmail.com>
2023-04-07 16:03:48 +0900

    config. #9 apollo client 이용하여 request 요청시에 항상 헤더에 token 포함하도록 설정 (2023.04.07)

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,33 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/',
+});
+
+//request시 항상 header에 token을 포함하도록 설정
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      token: token,
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -27,3 +45,5 @@ root.render(
     </ApolloProvider>
   </React.StrictMode>
 );
+
+
