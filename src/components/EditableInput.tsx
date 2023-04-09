ad469bd7904e375aa02f2a66a0d645bcaa078375
anna <te1902@gmail.com>
2023-04-09 15:36:54 +0900

    feat. #11 EditableInput 컴포넌트 useCallback 삭제 (2023.04.09)

--- src/components/EditableInput.tsx
+++ src/components/EditableInput.tsx
@@ -21,12 +21,20 @@ function EditableInput({
     const [value, setValue] = useState(initialValue);
 
     const handleInputChange = useCallback(function (e : ChangeEvent<HTMLInputElement>) {
+        console.log('handleInputChange');
         setValue(e.target.value);
     },[]);
 
-    const handleInputBlur = useCallback(()=>{
+
+    // Bad Code )
+    // const handleInputBlur = useCallback(()=>{
+    //     updateFn(value); //value는 초기값만 계속 기억하고 있으므로(자기가 선언된 시점의 함수의 변수값) setValue 이후에 변화된 값을 가져오지 못한다.
+    // },[]);
+
+    // Good Code )
+    const handleInputBlur = ()=>{
         updateFn(value);
-    },[]);
+    };
 
     return <EditStyleInput value={value} onChange={handleInputChange} onBlur={handleInputBlur}  />
 }
